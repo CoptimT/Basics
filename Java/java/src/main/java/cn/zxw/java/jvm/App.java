6243f0770617a0ee11c0ef52da7eef2d8cefe3d4
zhang xiangwei <zhangxw17@lenovo.com>
2018-09-03 20:49:48 +0800

    java

---
+++ Java/java/src/main/java/cn/zxw/java/jvm/App.java
@@ -0,0 +1,7 @@
+package cn.zxw.java.jvm;
+
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
---
+++ Java/java/src/test/java/cn/zxw/java/AppTest.java
@@ -0,0 +1,7 @@
+package cn.zxw.java;
+
+public class AppTest {
+	public static void main(String[] args) {
+
+	}
+}
