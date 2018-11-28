c88af2953076fea3535332efe6b218394c3e1990
CoptimT <zxw644@163.com>
2018-11-28 17:23:26 +0800

    tsdb put string

---
+++ Java/application/src/main/java/cn/zxw/application/tsdb/TsdbPutString.java
@@ -0,0 +1,157 @@
+package cn.zxw.application.tsdb;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+
+import cn.zxw.application.utils.HttpUtil;
+
+/**
+ * TSDB Put 压力测试
+ * 
+ * 该类特点：
+ * 1.可以指定线程数，metric数，tag数，三者相乘即每秒压力
+ * 2.支持Int,String
+ * 3.一个时序，只对应一个metric一个tag
+ * 4.tag数：每个metric标签数也即时序数，同时是每个PUT请求DataPoint数
+ * 
+ * 数据格式：
+ * metric = metricPrifix + "thd" + thdIndex + "mtc" + metricIndex
+ * tagk: host, tagv: web + tagIndex
+ * 
+ * java -jar test.jar http://172.17.171.15:4242/api/put?details 2 10 50 1 device
+ * url threadNum metricNum tagNum seconds metric dataType
+ */
+public class TsdbPutString {
+	//默认参数
+	static String url = "http://localhost:4242/api/put?details";
+	static int threadNum = 1;
+	static int metricNum = 1;
+	static int tagNum = 1;
+	static int seconds = 60;
+	static String metricPrifix = "Temperature";
+	static String dataType = "int";
+	
+	static Random random=new Random();
+	
+	public static void main(String[] args) {
+		try {
+			//参数传递
+			/*url = args[0];
+			threadNum = Integer.parseInt(args[1]);
+			metricNum = Integer.parseInt(args[2]);
+			tagNum = Integer.parseInt(args[3]);
+			seconds = Integer.parseInt(args[4]);
+			metricPrifix = args[5];
+			dataType = args[6];*/
+			//自定义
+			url = "http://172.17.171.15:8300/put?details";
+			threadNum = 10;
+			metricNum = 10;
+			tagNum = 10;
+			seconds = 600;
+			metricPrifix = "Temp";
+			dataType = "string";
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		}finally {
+			System.out.println(url 
+					+"\r\n - threadNum="+ threadNum 
+					+"\r\n - metricNum="+ metricNum 
+					+"\r\n - tagNum="+ tagNum 
+					+"\r\n - seconds="+ seconds 
+					+"\r\n - metricPrifix="+ metricPrifix
+					+"\r\n - dataType="+ dataType);
+		}
+		// 请求头
+		Map<String, String> headers = new HashMap<String, String>();
+		headers.put("Content-Type", "application/json");
+		//获取线程编号
+		final AtomicInteger threadIndex = new AtomicInteger(0);
+		
+		for (int t = 0; t < threadNum; t++) {
+			Thread thread = new Thread(new Runnable() {
+				public void run() {
+					int thdIndex = threadIndex.incrementAndGet();
+					long starttime = System.currentTimeMillis()/1000;
+					long timestamp = System.currentTimeMillis()/1000;
+					
+					int top = 100;
+					int bottom = 5;
+					int flag = 1;  //-1 数值降, 1数值升
+					
+					while((timestamp - starttime) < seconds) {
+						int value = 50;//bottom < value < top
+						if(dataType.equals("int")) {//确定写入value大小，模拟数值周期波动，而不是随机值
+							int change = random.nextInt(3);
+							if(flag > 0) {
+								value = value+change;
+								if(value > top) {
+									value = top;
+									flag = -1;
+								}
+							}else {
+								value = value-change;
+								if(value < bottom) {
+									value = bottom;
+									flag = 1;
+									top = random.nextInt(100);
+								}
+							}
+						}
+						
+						for (int metricIndex = 0; metricIndex < metricNum; metricIndex++) {
+							String metric = metricPrifix + "thd" + thdIndex + "mtc" + metricIndex;
+							JSONArray arr = new JSONArray();
+							for (int tagIndex = 0; tagIndex < tagNum; tagIndex++) {
+								JSONObject json = new JSONObject();
+								json.put("metric", metric);
+								json.put("timestamp", timestamp);
+								if(dataType.equals("int")) {
+									json.put("value", value);
+								}else if(dataType.equals("string")) {
+									json.put("type", "string");
+									json.put("value", metric);
+								}
+								JSONObject tags = new JSONObject();
+								tags.put("host", "web"+tagIndex);
+								json.put("tags", tags);
+								arr.add(json);
+							}
+							try {
+								List<String> res = HttpUtil.sendPostWithCode(url, arr.toJSONString(), headers);
+								if(!"200".equals(res.get(0))) {
+									System.out.println(metric + " - " + res.get(0)+" - " + res.get(1));
+								}
+							} catch (Exception e) {
+								System.out.println("Http Error: " + metric + " - " + e.getLocalizedMessage());
+							}
+						}
+						while(true) {
+							long now = System.currentTimeMillis()/1000;
+							if(now - timestamp > 1) {
+								System.out.println(Thread.currentThread().getName() + " put slow than expect...");
+							}
+							if(now - timestamp >= 1) {
+								timestamp = now;
+								break;
+							}
+							try {
+								Thread.sleep(500);
+							} catch (InterruptedException e) {
+							}
+						}
+					}
+					System.out.println(Thread.currentThread().getName() + " run "+ (System.currentTimeMillis()/1000 - starttime) +" seconds");
+				}
+			});
+			thread.start();
+		}
+	}
+
+}
